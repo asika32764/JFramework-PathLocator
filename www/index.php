1f82b063b81d88ec21ac0c40d2f0fdcb5165309b
Asika <asika32764@gmail.com>
2013-09-17 21:41:50 +0800

    Add files

---
+++ www/index.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define required paths
+define('JPATH_BASE',    dirname(__DIR__));
+define('JPATH_ROOT',    JPATH_BASE);
+define('JPATH_SOURCE',  JPATH_BASE . '/src');
+define('JPATH_WEB',     JPATH_BASE . '/www');
+
+// Load the Composer autoloader
+require JPATH_BASE . '/vendor/autoload.php';
+
